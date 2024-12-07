dcdcb4b28f66f99005aead4f7c7cebbf7c60e166
PiotrChyrc <43895326+PiotrChyrc@users.noreply.github.com>
2024-12-07 15:24:29 +0000

    Add files via upload

---
+++ Level 04 - Comments/The Thing Namer 3000/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); //This stores the noun of the sentence
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); /* This Stores the adjective describing the noun*/
+string c = "of Doom"; // This Stores text
+string d = "3000"; //This stores the version number
+Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
