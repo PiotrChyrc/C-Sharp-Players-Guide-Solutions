4c21dffc74e71c712f9a3d110cad1d1295459a62
PiotrChyrc <43895326+PiotrChyrc@users.noreply.github.com>
2024-12-07 14:38:45 +0000

    Add files via upload

---
+++ Level 03 - Hello World/The Makings of a Programmer/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello There");
+Console.WriteLine("General Kenobi!");
+Console.WriteLine("You are a bold one!");
+Console.WriteLine("That was a Star Wars Reference");
+Console.WriteLine("Did you get it?");
