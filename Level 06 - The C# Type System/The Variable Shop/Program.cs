d895716edea977b73b0852456b74bae15eb2ff78
PiotrChyrc <43895326+PiotrChyrc@users.noreply.github.com>
2024-12-09 17:03:20 +0000

    Add files via upload

---
+++ Level 06 - The C# Type System/The Variable Shop/Program.cs
@@ -0,0 +1,35 @@
+﻿int a = 5;
+uint b = 5;
+
+short c = 5;
+ushort d = 5;
+
+long e = 5;
+ulong f = 3;
+
+byte g = 5;
+sbyte h = 5;
+
+char i = 'A';
+string j = "AB";
+
+bool k = false;
+
+float l = 5.4f;
+double m = 3.8;
+decimal n = 5.5m;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(n);
