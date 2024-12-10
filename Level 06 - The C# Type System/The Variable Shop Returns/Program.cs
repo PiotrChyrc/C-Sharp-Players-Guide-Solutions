d5c55154d327d2ab50f0faedf9bae7dba207a32a
PiotrChyrc <43895326+PiotrChyrc@users.noreply.github.com>
2024-12-10 19:10:15 +0000

    Uploading completed exercise

---
+++ Level 06 - The C# Type System/The Variable Shop Returns/Program.cs
@@ -0,0 +1,51 @@
+﻿int a = 5;
+uint b = 5;
+
+short c = 5;
+ushort d = 5;
+
+long e = 5;
+ulong f = 3;
+
+byte g = 5;
+sbyte h = 5;
+
+char i = 'A';
+string j = "AB";
+
+bool k = false;
+
+float l = 5.4f;
+double m = 3.8;
+decimal n = 5.5m;
+
+a = 10;
+b = 10;
+c = 10;
+d = 10;
+e = 10;
+f = 10;
+g = 10;
+h = 10;
+i = 'Z';
+j = "ZA";
+k = true;
+l = 4.8f;
+m = 4.6;
+n = 5.4m;
+
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(n);
