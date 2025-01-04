427c3c3074fc407e5dead41da788543e91fc545a
PiotrChyrc <43895326+PiotrChyrc@users.noreply.github.com>
2025-01-04 16:03:20 +0000

    Uploading the completed exercise

---
+++ Level 07 - Math/The Four Sisters and the Duckbear/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("How many eggs?");
+string eggs = Console.ReadLine();
+int eggNumber = Convert.ToInt32(eggs);
+
+int sisterEggs = eggNumber / 4;
+int duckbearEggs = eggNumber % 4;
+
+Console.WriteLine("Each sister gets " + sisterEggs + " Eggs and the duckbear gets " + duckbearEggs);
