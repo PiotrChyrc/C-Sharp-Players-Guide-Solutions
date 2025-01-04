36b33fec5334846071c1b4fd961534e96e1efcec
PiotrChyrc <43895326+PiotrChyrc@users.noreply.github.com>
2025-01-04 15:44:01 +0000

    Uploading Triangle Farmer Solution

---
+++ Level 07 - Math/The Triangle Farmer/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Please enter base size");
+String Base = Console.ReadLine();
+Console.WriteLine("Please enter height");
+String Height = Console.ReadLine();
+
+float area = (Convert.ToSingle(Base) * Convert.ToSingle(Height) / 2 );
+Console.WriteLine("The Area is " + Convert.ToString(area));
