14089704011a96b91a47d2b647895b8588d6a43e
PiotrChyrc <43895326+PiotrChyrc@users.noreply.github.com>
2025-01-11 17:17:29 +0000

    Uploading completed task

---
+++ Level 07 - Math/The Dominion of Kings/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("How many Estates?");
+int enteredEstate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How many duchies?");
+int enteredDuchies = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How many provinces?");
+int enteredProvinces = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Total points: " + Convert.ToString((enteredDuchies * 3) + (enteredEstate * 1) + (enteredProvinces * 6)));
