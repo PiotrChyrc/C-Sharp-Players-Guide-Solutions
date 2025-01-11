aa0036509de5b2e29e8e03e6010f11b26f2fbf53
PiotrChyrc <43895326+PiotrChyrc@users.noreply.github.com>
2025-01-11 18:55:56 +0000

    Uploading complete task

---
+++ Level 09 - Decision Making/Repairing the Clocktower/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Please give a number ");
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+if (inputNumber % 2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+
+else
+{
+    Console.WriteLine("Tock");
+}
