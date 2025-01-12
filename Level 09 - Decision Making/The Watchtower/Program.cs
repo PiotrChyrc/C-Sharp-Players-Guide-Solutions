71e3d0bd611946c503270731674af60a17d18041
PiotrChyrc <43895326+PiotrChyrc@users.noreply.github.com>
2025-01-12 12:00:29 +0000

    Add files via upload

---
+++ Level 09 - Decision Making/The Watchtower/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("What is the value of X? ");
+int xValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("What is the value of Y? ");
+int yValue = Convert.ToInt32(Console.ReadLine());
+
+if (xValue > 0 && yValue > 0) Console.WriteLine("The enemy is to the northeast!");
+if (xValue > 0 && yValue == 0) Console.WriteLine("The enemy is to the east!");
+if (xValue > 0 && yValue < 0) Console.WriteLine("The enemy is to the southeast!");
+if (xValue == 0 && yValue > 0) Console.WriteLine("The enemy is to the north!");
+if (xValue == 0 && yValue == 0) Console.WriteLine("The enemy is here!");
+if (xValue == 0 && yValue < 0) Console.WriteLine("The enemy is to the south!");
+if (xValue < 0 && yValue > 0) Console.WriteLine("The enemy is to the northwest!");
+if (xValue < 0 && yValue == 0) Console.WriteLine("The enemy is to the west!");
+if (xValue < 0 && yValue < 0) Console.WriteLine("The enemy is to the southwest!");
