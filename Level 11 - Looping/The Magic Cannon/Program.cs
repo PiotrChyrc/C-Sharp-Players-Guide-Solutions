fcb1f4511754f5091404ac30a7fd910d516162b5
PiotrChyrc <43895326+PiotrChyrc@users.noreply.github.com>
2025-01-19 16:40:47 +0000

    Uploading Completed Exercise

---
+++ Level 11 - Looping/The Magic Cannon/Program.cs
@@ -0,0 +1,23 @@
+﻿for (int i = 1; i <=100; i++)
+{
+    if (i % 3 == 0 && i % 5 ==0)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine($"{i}: Combined Blast!");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{i}: Fire");
+    }
+    else if ( i % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{i}: Electric");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine($"{i}: Normal");
+    }
+}
