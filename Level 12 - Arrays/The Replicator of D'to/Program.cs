836f1436175b3b84ac64c6e486ca83855064c2db
PiotrChyrc <43895326+PiotrChyrc@users.noreply.github.com>
2025-01-25 16:39:07 +0000

    Add files via upload

---
+++ Level 12 - Arrays/The Replicator of D'to/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+int[] array = new int[5];
+Console.WriteLine("Please enter 5 numbers ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Please enter number {i + 1}");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] secondArray = new int[5];
+
+for  (int i = 0; i < secondArray.Length;i++)
+{
+    secondArray[i] = array[i];
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{array[i]} and {secondArray[i]}");
+}
