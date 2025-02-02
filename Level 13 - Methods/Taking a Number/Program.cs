c39eea07b95c55d2591600faf0739215004118fb
PiotrChyrc <43895326+PiotrChyrc@users.noreply.github.com>
2025-02-02 17:14:12 +0000

    Uploading completed methods

---
+++ Level 13 - Methods/Taking a Number/Program.cs
@@ -0,0 +1,21 @@
+﻿int result = AskForNumber("What is the airspeed velocity of an unladen swallow?");
+
+int AskForNumber(string text)
+{
+    Console.WriteLine($"{text} ");
+    int convertedText = Convert.ToInt32(Console.ReadLine());
+    return convertedText;
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        int number = AskForNumber("Please give a number");
+
+        if (number >= min && number <= max)
+        {
+            return number;
+        }    
+    }
+}
